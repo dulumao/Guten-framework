705e6c0896675a47ddcb43ca92986298dc524d3e
Matt Ma <395241021@qq.com>
2019-07-08 17:00:20 +0800

    update

--- app/core/adapter/validation/validation.go
+++ app/core/adapter/validation/validation.go
@@ -94,7 +94,8 @@ func (self *Validation) Regexp(str string, pattern string) bool {
 
 func (self *Validation) addValidations() {
 	validate.AddValidator("phone", func(val string) bool {
-		reg := regexp.MustCompile(`^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,3,5,6,7,8]{1}\d{8}$|^18[\d]{9}$`)
+		// reg := regexp.MustCompile(`^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,3,5,6,7,8]{1}\d{8}$|^18[\d]{9}$`)
+		reg := regexp.MustCompile(`^1\d{10}$`)
 
 		return reg.MatchString(val)
 	})
